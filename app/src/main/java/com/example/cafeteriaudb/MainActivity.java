febb3bd2e85d0db029f32f2a1898f13dd259c467
AbigaailAcuna <acuaabigail@yahoo.com>
2024-04-22 16:57:05 -0600

    Pantalla de Login

--- app/src/main/java/com/example/cafeteriaudb/MainActivity.java
+++ app/src/main/java/com/example/cafeteriaudb/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.cafeteriaudb;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.content.Intent;
 import com.example.cafeteriaudb.adapters.MainAdapter;
 import com.example.cafeteriaudb.modelos.MainModel;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MainAdapter mainAdapter;
+
+    TextView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +34,22 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("Menu");
 
         FirebaseRecyclerOptions<MainModel> options = new FirebaseRecyclerOptions.Builder<MainModel>()
-                .setQuery(databaseRef.child("Cena").orderByChild("Plato"), MainModel.class)
+                .setQuery(databaseRef.child("Almuerzos").orderByChild("Plato"), MainModel.class)
                 .build();
 
         mainAdapter = new MainAdapter(options);
         recyclerView.setAdapter(mainAdapter);
+
+
+        menu = findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar LoginActivity cuando se haga clic en el botón
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
