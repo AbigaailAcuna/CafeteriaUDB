b6ac6fbabed134372c879ad97b09eebe97f59282
LuisDC2001 <101295959+LuisDC2001@users.noreply.github.com>
2024-05-10 21:15:42 -0600

    Agregando pantalla Splash

---
+++ app/src/main/java/com/example/cafeteriaudb/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.cafeteriaudb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animaciones
+        Animation animacion1= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+        TextView cafeteriaTextView = findViewById(R.id.txtCafeteria);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        cafeteriaTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 4000);
+
+    }
+}
